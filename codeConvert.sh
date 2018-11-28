31185f04bd5ac8d048ecc897956dbbc6dc62b4af
ChenJiayi <472816717@qq.com>
2018-11-28 09:11:53 +0800

    隐藏签名和图标，修改工程名

---
+++ codeConvert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+file=newest.csv
+filterFile=filter.txt
+IFS=","
+while read name icon group activity type null
+do
+	echo "<item
+	component=\"ComponentInfo{$group/$activity}\"
+	drawable=\"$icon\"/>
+" >> $filterFile
+	echo "<item>$icon</item>" >> "$type.txt"
+done < $file
