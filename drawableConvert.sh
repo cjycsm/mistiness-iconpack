148896fb53e3f553c7e757c0c73cbfc58057fb2f
cjycsm <472816717@qq.com>
2018-12-03 17:07:16 +0800

    优化图标命名

--- drawableConvert.sh
+++ drawableConvert.sh
@@ -4,7 +4,7 @@ path=./MistinessIconpack/app/src/main/res/drawable-nodpi
 files=$(ls $path)
 for filename in $files
 do
-	if [ $filename == "headerimage.png" ]; then
+	if [ $filename == "header_image.png" ]; then
 		continue
 	fi
 	echo "<item drawable=\"${filename%.*}\"/>" >> drawable.txt
