20a2e8729db1562c166f0a7d6ea8b9dd2cde74ff
cjycsm <472816717@qq.com>
2019-03-16 21:06:18 +0800

    1.4.8: 新增15个图标

---
+++ packageConvert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir ./flyme/
+
+IFS=","
+while read name icon package activity type
+do
+	if ! [[ $type =~ "魅族" ]] && ! [[ $type =~ "应用" ]] && ! [[ $type =~ "游戏" ]]; then
+		continue
+	fi
+	cp "$icon.png" "./flyme/$package.png"
+done < "_a.csv"
+
+read -p "按任意键继续"
