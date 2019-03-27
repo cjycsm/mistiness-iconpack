3e62c83f7d52316a014b5a1aedc53304e7299def
cjycsm <472816717@qq.com>
2019-03-27 23:03:03 +0800

    1.5.5: 新增35个图标，重绘5个图标

--- drawableConvert.sh
+++ drawableConvert.sh
@@ -4,7 +4,7 @@ path=./MistinessIconpack/app/src/main/res/drawable-nodpi
 files=$(ls $path)
 for filename in $files
 do
-	if [ $filename == "header_image.png" ] || [ $filename == "app_logo.png" ] || [[ $filename =~ "dynamic_" ]] || [[ $filename =~ "clock_" ]]; then
+	if [ $filename == "header_image.png" ] || [ $filename == "app_logo.png" ] || [[ $filename == "dynamic_"* ]] || [[ $filename == "clock_"* ]] || [[ $filename =~ "calendar_"[0-9]{1,} ]] || [[ $filename =~ "google_calendar_"[0-9]{1,} ]]; then
 		continue
 	fi
 	echo "<item drawable=\"${filename%.*}\"/>" >> drawable.txt
